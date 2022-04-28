dadc0d4d1bbc4c8b62a4661d3dcdf0dc03624fe9
rinapyktina <91655367+rinapyktina@users.noreply.github.com>
2022-04-28 08:15:30 +0300

    Update 12.04.3.py

--- 12.04.3.py
+++ 12.04.3.py
@@ -1,4 +1,9 @@
-#ввод матрицы в файл
+# !/usr/bin/env python3
+# /-*- coding: UTF-8 -*-
+
+if __name__ == "__main__":
+    n = int(input("n - "))
+    m = int(input("m - "))
 A = [...]
 with open("matrix.txt", "w", encoding="utf-8") as file:
     for row in A:
